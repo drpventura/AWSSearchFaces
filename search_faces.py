6ff96c13730530329000c765adb63c4965ac5dca
Philip R. Ventura, Ph.D <pventura@usf.edu>
2019-03-05 14:01:48 -0500

    Fixed return value in the case when no face is found to be of type string. Source code reformmatting.

--- search_faces.py
+++ search_faces.py
@@ -4,7 +4,8 @@ from pprint import pprint
 from image_helpers import get_image
 from typing import List
 
-def delete_collection(coll_name:str):
+
+def delete_collection(coll_name: str):
     """
     Attempts to delete the specified collection.
     Raises an error if the collection does not exist.
@@ -49,7 +50,7 @@ def list_collections() -> List[str]:
     return result
 
 
-def collection_exists(coll_name:str) -> bool:
+def collection_exists(coll_name: str) -> bool:
     """
     Checks to see if the collection exists
     :param coll_name: the name of the collection to check
@@ -58,7 +59,7 @@ def collection_exists(coll_name:str) -> bool:
     return coll_name in list_collections()
 
 
-def create_collection(coll_name:str):
+def create_collection(coll_name: str):
     """
     Creates a collection with the specified name, if it does not already exist
     :param coll_name: the name of the collection to create
@@ -75,7 +76,7 @@ def create_collection(coll_name:str):
                   + ', status code: ' + str(response['StatusCode'])
 
 
-def list_faces(coll_name:str) -> List[dict]:
+def list_faces(coll_name: str) -> List[dict]:
     """
     Return a list of faces in the specified collection.
     :param coll_name: the collection.
@@ -105,7 +106,7 @@ def list_faces(coll_name:str) -> List[dict]:
     return result
 
 
-def add_face(coll_name:str, image:str):
+def add_face(coll_name: str, image: str):
     """
     Adds the specified face image to the specified collection.
     :param coll_name: the collection to add the face to
@@ -117,7 +118,7 @@ def add_face(coll_name:str, image:str):
     # last access 3/5/2019
 
     # nested function
-    def extract_filename(fname_or_url:str) -> str:
+    def extract_filename(fname_or_url: str) -> str:
         """
         Returns the last component of file path or URL.
         :param fname_or_url: the filename or url.
@@ -136,41 +137,38 @@ def add_face(coll_name:str, image:str):
         raise Exception('No face found in the image')
 
 
-def find_face_id(coll_name:str, ext_img_id:str) -> str:
+def find_face_id(coll_name: str, ext_img_id: str) -> str:
     """
     Find the face_id for the specified image in the collection.
     :param coll_name: the name of the collection.
     :param ext_img_id: the ExternalImageId set for the image
-    :return: the ImageId if found, or None otherwise
+    :return: the ImageId if found, or the emtpy string otherwise
     """
     face = [face for face in list_faces(coll_name) if face['ExternalImageId'] == ext_img_id]
     if face != []:
         return face[0]['ImageId']
     else:
-        return None
+        return ''
 
 
-def delete_face(coll_name: str, face_ids:List[str]):
+def delete_face(coll_name: str, face_ids: List[str]) -> str:
     """
     Deletes the specified faces from the collection.
     :param coll_name: the name of the collection
     :param face_ids: a list of face ids (see FaceId) field in collection
+    :return: returns a list of the face ids that were deleted
     """
     # lightly edited version of
     # https://docs.aws.amazon.com/rekognition/latest/dg/delete-faces-procedure.html
     # last access 3/5/2019
 
     client = boto3.client('rekognition')
-
     response = client.delete_faces(CollectionId=coll_name,
                                    FaceIds=face_ids)
-
-    print(str(len(response['DeletedFaces'])) + ' faces deleted:')
-    for faceId in response['DeletedFaces']:
-        print(faceId)
+    return response['DeletedFaces']
 
 
-def find_face(coll_name:str, face_to_find:str) -> List[dict]:
+def find_face(coll_name: str, face_to_find: str) -> List[dict]:
     """
     Searches for the specified face in the collection.
     :param face_to_find: a string that is either the filename or URL to the image containing the face to search for.
@@ -200,3 +198,7 @@ pprint(list_faces('Faces'))
 
 # pprint(find_face('Faces', 'https://m.media-amazon.com/images/M/MV5BNDExMzIzNjk3Nl5BMl5BanBnXkFtZTcwOTE4NDU5OA@@._V1_UX214_CR0,0,214,317_AL_.jpg'))
 pprint(find_face('Faces', r'images\tampabay.jpg'))
+
+face_ids = [face['FaceId'] for face in list_faces('Faces')]
+pprint(face_ids)
+delete_face('Faces', face_ids)
